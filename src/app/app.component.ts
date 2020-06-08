512dc54d49a379ed6b6e25952c645b855390cafc
maksymzav <maksym.zavorotynskyi@gmail.com>
2020-06-08 10:56:37 +0300

    add a table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
+import {PageEvent} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'paginator-intl';
+export class AppComponent implements OnInit {
+  displayedColumns = ['index', 'name'];
+  datasource: MatTableDataSource<any>;
+  data = [];
+
+  pageSizeOptions = [10, 15, 25, 50, 100];
+  pageSize = 15;
+  currentPage = 0;
+
+  ngOnInit() {
+    for (let index = 0; index < 1059; index++) {
+      this.data.push({index, name: 'name' + index});
+    }
+
+    this.datasource = new MatTableDataSource(this.data.slice(0, this.pageSize));
+  }
+
+  pageChanged(event: PageEvent){
+    const start = event.pageIndex * event.pageSize;
+    const end = Math.min(start + event.pageSize, this.data.length -1);
+    this.datasource = new MatTableDataSource(this.data.slice(start, end));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
